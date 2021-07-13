5e2d0c254a1881a3ffc75d40ae5699dce2c3ee28
Potato1682 <contact@potato1682.ml>
2021-07-14 06:27:13 +0900

    feat: threadpool tcp server

--- src/server/server.py
+++ src/server/server.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 from socketserver import ThreadingTCPServer
 from run import Main
 from server.handler import Handler
@@ -15,12 +16,24 @@ class Server(ThreadingTCPServer, object):
     instance: Main
     thread: threading.Thread
 
+    def __init__(self, address, handler, workers):
+        super().__init__(address, handler)
+
+        self.executor = ThreadPoolExecutor(max_workers=workers)
+
+    def process_request(self, request, client_address):
+        self.executor.submit(self.process_request_thread, request, client_address)
+
+    def server_close(self):
+        super().server_close()
+        self.executor.shutdown(wait=True)
+
     def server_bind(self):
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind(self.server_address)
 
 def startServer(instance, port, logger, token):
-    with Server(("", port), Handler) as server:
+    with Server(("", port), Handler, 4) as server:
         server.logger = logger
         server.token = token
         server.instance = instance
@@ -31,7 +44,7 @@ def startServer(instance, port, logger, token):
         except SystemExit:
             pass
         finally:
-            server.shutdown()
+            server.server_close()
 
 
 def bind(port, instance, logger, token):
