3c1db01c434c40ae7411aa252ef63d6250028bb3
Potato1682 <contact@potato1682.ml>
2021-07-14 05:46:32 +0900

    fix: make thread daemon mode

--- src/server/server.py
+++ src/server/server.py
@@ -1,25 +1,42 @@
 from socketserver import ThreadingTCPServer
+from run import Main
 from server.handler import Handler
 import socket
 from ssl import wrap_socket
 import threading
 
+from utils.logging import Logger
+from utils.token import Token
+
 
 class Server(ThreadingTCPServer, object):
+    logger: Logger
+    token: Token
+    instance: Main
+    thread: threading.Thread
+
     def server_bind(self):
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind(self.server_address)
 
-
 def startServer(instance, port, logger, token):
-    server = Server(("", port), Handler)
-    server.logger = logger
-    server.token = token
-    server.instance = instance
-    server.serve_forever()
+    with Server(("", port), Handler) as server:
+        server.logger = logger
+        server.token = token
+        server.instance = instance
+        server.allow_reuse_address = True
+
+        try:
+            server.serve_forever()
+        except SystemExit:
+            pass
+        finally:
+            server.shutdown()
 
 
 def bind(port, instance, logger, token):
-    t = threading.Thread(target=startServer, args=(instance, port, logger,  token))
-    t.start()
+    thread = threading.Thread(target=startServer, args=(instance, port, logger, token))
+    thread.daemon = True
+    thread.start()
     logger.info("server", "Listening on 0.0.0.0:" + str(port))
+
