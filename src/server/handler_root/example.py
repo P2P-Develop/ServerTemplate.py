63b612ced0176b44b03b368cd523822b75cdd408
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-08-24 07:05:42 +0900

    style: 整形

--- src/server/handler_root/example.py
+++ src/server/handler_root/example.py
@@ -3,19 +3,19 @@ import route
 
 # /example
 
-
+@route.require_auth  # This endpoint requires authorization.
 def handle(handler, path, params):
-    result.success(handler, 200, "It's working!")
+    route.success(handler, 200, "It's working!")
 
 
 def genDoc():
     return {
         "get": {
             "about": "Example document.",
-            "returns": "text/plain",           # Return mime type.
-            200: {                             # HTTP Code
+            "returns": "text/plain",  # Return mime type.
+            200: {  # HTTP Code
                 "about": "Example response.",
-                "example": "It works!"         # Objects, texts, numbers, and boolean values can be set.
+                "example": "It works!"  # Objects, texts, numbers, and boolean values can be set.
             }
         }
     }
