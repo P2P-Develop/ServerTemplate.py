09c54f5c668ac6d8d117d592af525dd9d955c686
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-09-03 16:05:40 +0900

    docs: Add document to docs endpoint

--- resources/docs.py
+++ resources/docs.py
@@ -4,7 +4,12 @@ from endpoint import *
 import route
 
 
-@http("GET", require_auth=False)
+@http("GET", require_auth=False,
+      docs=Document("View document.",
+                    types="text/html",
+                    responses=[
+                        Response(200, "Successful response.")
+                    ]))
 def on_get(handler, params):
     if not os.path.exists("docs.html"):
         route.post_error(handler, route.Cause.RESOURCE_NOTFOUND)
