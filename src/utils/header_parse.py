7b79475231d2d2602ae9f6dce6e7b3b1441c5866
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-09-06 19:13:21 +0900

    feat: MultiValueHeader

--- src/utils/header_parse.py
+++ src/utils/header_parse.py
@@ -15,16 +15,49 @@ class DecoratedHeader(Header):
     def __init__(self, name, value):
         super().__init__(name, value)
         self.decoration = {}
+        self.raw_value = value
         self._parse()
 
     def _parse(self):
         dc = self.value.split(";")
+        if len(dc) < 1:
+            self.raw_value = dc[0]
         for dv in dc:
             kv = dv.split("=", 1)
-            if len(kv) != 2:
-                self.decoration = {}
+            if len(kv) == 2:
+                self.decoration[_(kv[0])] = None
+                continue
+            elif len(kv) == 0:
                 continue
             self.decoration[_(kv[0])] = _(kv[1])
+
+    def __eq__(self, other):
+        return self.raw_value.lower() == other.lower()
+
+    def __contains__(self, item):
+        return item.lower() in self.decoration
+
+
+class MultiValueHeader(Header):
+    def __init__(self, name, value):
+        super().__init__(name, "")
+        self.value = {}
+        self._parse(value)
+
+    def _parse(self, value):
+        dc = value.split(",")
+        for dv in dc:
+            cn = _(dv).lower()
+            if ";" in value:
+                self.value[cn] = DecoratedHeader(self.name, dv)
+                continue
+            self.value[cn] = Header(self.name, dv)
+
+    def __eq__(self, other):
+        return other.lower() in self.value
+
+    def __contains__(self, item):
+        return item.lower() in self.value
 
 
 class HeaderSet:
@@ -36,9 +69,12 @@ class HeaderSet:
             self.add(*kv)
 
     def add(self, name, value):
+        if "," in value:
+            self._headers[_(name.lower())] = MultiValueHeader(name, value)
+            return
         if ";" in value:
             self._headers[_(name.lower())] = DecoratedHeader(name, value)
-
+            return
         self._headers[_(name.lower())] = Header(name, value)
 
     def get(self, name):
