05fa487132cd237d8823cc881da1ed0990f595f0
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-11-15 22:43:50 +0900

    feat: Collapsed stack traece

--- src/utils/stacktrace.py
+++ src/utils/stacktrace.py
@@ -7,11 +7,29 @@ def get_stack_trace(base_module, exception_type, exception, trace):
 
     st = f"Unexpected exception while handling client request resource '{path}'\n"
 
+    caused_by_reached = False
+    last_caused_by = None
+    duplicate_count = 0
+
     for stack in tb.stack:
         stack: traceback.FrameSummary
-        if base_module in stack.filename:
+        if base_module in stack.filename and not caused_by_reached:
             st += "Caused by: " + get_class_chain(exception_type) + ": " + str(tb) + "\n"
-        st += build_trace(stack)
+            caused_by_reached = True
+
+        built_trace = build_trace(stack)
+
+        if built_trace == last_caused_by:
+            duplicate_count += 1
+            continue
+        elif duplicate_count > 0:
+            st += f"\t... and {duplicate_count} more\n"
+            duplicate_count = 0
+            continue
+
+        duplicate_count = 0
+        st += built_trace
+        last_caused_by = built_trace
     return st
 
 
